46ba71f04d15075d7ad840cff42a75a6001273ad
Musaddek Ali <mussali.one@gmail.com>
2024-02-02 14:41:57 +0000

    make get sales data function a loop until valid is input

--- run.py
+++ run.py
@@ -13,24 +13,26 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
-
 data = sales.get_all_values()
-
 
 def get_sales_data():
     """
     Get sales figutes input from the user
     """
+    while True:
+        print("Please enter sales data from last market.")
+        print("Data should be six numbers, seperated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    print("Please enter sales data from last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+        data_str = input("Enter your data here: ")
+        
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
-
+    return sales_data
 
 def validate_data(values):
     """
@@ -46,6 +48,8 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
+    return True
 
-get_sales_data()
+data = get_sales_data()
